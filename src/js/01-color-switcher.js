d1734be459d4e192b9b41f3f78f3fd61669748fd
roel.edeleon <roel.edeleon@gmail.com>
2024-02-18 22:52:47 +0800

    Initial Commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,53 @@
+// DECLARATIONS ----------
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+bgColorChangeInterval = null;
+isBgColorChangeEnabled = false;
+
+btnStart.removeAttribute('disabled');
+btnStop.setAttribute('disabled', true);
+
+// FUNCTIONS ----------
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function checkIsBgChangeColorEnabled() {
+  if (isBgColorChangeEnabled) {
+    // Start Interval
+    bgColorChangeInterval = setInterval(() => {
+      changeBgColor();
+    }, 1000);
+  } else {
+    // Stop Interval
+    clearInterval(bgColorChangeInterval);
+  }
+}
+
+const changeBgColor = data => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+// EVENT LISTENERS ----------
+
+btnStart.addEventListener('click', () => {
+  isBgColorChangeEnabled = true;
+  checkIsBgChangeColorEnabled();
+
+  // START-STOP Button Settings
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+});
+
+btnStop.addEventListener('click', () => {
+  isBgColorChangeEnabled = false;
+  checkIsBgChangeColorEnabled();
+
+  // START-STOP Button Settings
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+});
